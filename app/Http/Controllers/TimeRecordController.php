7b019e8d896051f310dbfec6b58f6cfa1ad46a24
Shoko S <fruitmachine0@gmail.com>
2018-03-10 00:48:37 +0900

    モデル、コントローラ

---
+++ app/Http/Controllers/TimeRecordController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TimeRecord;
+use Illuminate\Http\Request;
+
+class TimeRecordController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(TimeRecord $timeRecord)
+    {
+    }
+
+    public function edit(TimeRecord $timeRecord)
+    {
+    }
+
+    public function update(Request $request, TimeRecord $timeRecord)
+    {
+    }
+
+    public function destroy(TimeRecord $timeRecord)
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/time_records', 'TimeRecordController', ['only' => [
+  'index', 'store', 'create', 'update'
+]])->middleware('auth');
