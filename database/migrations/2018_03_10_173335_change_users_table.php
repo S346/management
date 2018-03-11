a05cd4ec59d346f03230786bf9ccc24fb51c9d45
Shoko S <fruitmachine0@gmail.com>
2018-03-11 19:41:01 +0900

    マイグレーションファイル変更

--- database/migrations/2018_03_10_173335_change_users_table.php
+++ database/migrations/2018_03_10_173335_change_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIsActiveToUsersTable extends Migration
+class ChangeUsersTable extends Migration
 {
     public function up()
     {
--- database/migrations/2018_03_10_195905_create_projects_table.php
+++ database/migrations/2018_03_10_195905_create_projects_table.php
@@ -11,10 +11,7 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('user_id')->unsigned();
             $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
