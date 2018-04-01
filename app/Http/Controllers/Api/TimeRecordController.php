bc3a8194aabb93ec471fa2027c4b2d1f3ec9cf1b
Sanshiro Sakai <xosya3@gmail.com>
2018-04-01 18:53:49 +0900

    apiをパラメーターで検索できるように修正

--- app/Http/Controllers/Api/TimeRecordController.php
+++ app/Http/Controllers/Api/TimeRecordController.php
@@ -5,11 +5,29 @@ namespace App\Http\Controllers\Api;
 use Auth;
 use App\TimeRecord;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class TimeRecordController extends Controller {
-    public function index() {
-        $time_records = TimeRecord::get();
+    public function index(Request $request) {
+        $time_record_query = TimeRecord::query();
+
+        $where_condition = array_filter([
+            'user_id' => $request->input('user_id'),
+            'project_id' => $request->input('project_id'),
+        ]);
+
+        if ($where_condition) {
+            $time_record_query = $time_record_query->where($where_condition);
+        }
+
+        $start_month = $request->input('start_month');
+
+        if(is_numeric($start_month)) {
+            $time_record_query = $time_record_query->whereMonth('start_at', $start_month);
+        };
+
+        $time_records = $time_record_query->get();
         return response()->json($time_records);
     }
 }
